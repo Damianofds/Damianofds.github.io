11ca53641c91f8d324be44a5ff8aba85abac424e
Damiano Giampaoli <damianofds@gmail.com>
2016-01-07 20:27:19 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
             var email = $("input#email").val();
             var ccopy = $("input#ccopy").is(':checked') ? "checked" : "";
             var message = $("textarea#message").val();
-            var hiddenRecaptcha = $("input#hiddenRecaptcha").val();
+            var recaptcha-res = $("#g-recaptcha-response").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -31,7 +31,7 @@ $(function() {
                     email: email,
                     text: message,
                     carboncopy: ccopy,
-                    hiddenRecaptcha: hiddenRecaptcha
+                    grecaptcharesponse: recaptcha-res
                 },
                 cache: false,
                 success: function() {
